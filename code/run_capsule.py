76b79a6ec66db9398351fcc6e412cb77344b91ee
bjhardcastle <ben.hardcastle@alleninstitute.org>
2025-03-19 04:29:33 +0000

    Fix syntax error

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -50,14 +50,14 @@ class CapsuleParameters(pydantic_settings.BaseSettings):
         # the order of the sources is what defines the priority:
         # - first source is highest priority
         # - for each field in the class, the first source that contains a value will be used
-        json_file = next(pathlib.Path('/data/parameters').glob('*_input_parameters*.json'), None))
+        json_file = next(pathlib.Path('/data/parameters').glob('*_input_parameters*.json'), None)
         if json_file is not None:
             logger.info(f"Using parameters file: {json_file}") 
         else:
             logger.info(f"No parameters file found")
         return (
             init_settings,
-            pydantic_settings.sources.JsonConfigSettingsSource(settings_cls, json_file=json_file,
+            pydantic_settings.sources.JsonConfigSettingsSource(settings_cls, json_file=json_file),
             pydantic_settings.CliSettingsSource(settings_cls, cli_parse_args=True),
         )
 
